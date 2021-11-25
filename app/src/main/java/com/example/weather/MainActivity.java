468f34abc0e2ac9a211103f9b7666de0fe9565b2
Shanshan Zhao <shanshanzhao@Shanshans-MacBook-Pro.local>
2021-11-25 15:43:07 -0500

    Works

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,14 +1,151 @@
 package com.example.weather;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView weatherTextView;
+    EditText cityEditText;
+    EditText stateEditText;
+
+    String apiUrl;
+
+
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Could not find weather :(",Toast.LENGTH_SHORT).show();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+
+                String weatherInfo = jsonObject.getString("weather");
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                String msg = "";
+
+                for (int i=0; i < arr.length(); i++) {
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    String main = jsonPart.getString("main");
+                    String description = jsonPart.getString("description");
+
+                    if (!main.equals("") && !description.equals("")) {
+                        msg += main + ": " + description + "\r\n";
+                    }
+                }
+
+                if (!msg.equals("")) {
+                    weatherTextView.setText(msg);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Could not find weather :(",Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Could not find weather :(",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void click (View view) {
+
+        try {
+            DownloadTask task = new DownloadTask();
+
+            String encodedCityName = URLEncoder.encode(cityEditText.getText().toString(), "UTF-8");
+            String encodedStateCode = URLEncoder.encode(stateEditText.getText().toString(), "UTF-8");
+
+            apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName + "," + encodedStateCode + "&appid=62e64f33d1669af09865f6d4c9d9edd6";
+
+            try {
+                task.execute(apiUrl);
+                Log.i("apiUrl", apiUrl);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            mgr.hideSoftInputFromWindow(cityEditText.getWindowToken(), 0);
+            mgr.hideSoftInputFromWindow(stateEditText.getWindowToken(), 0);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Could not find weather :(",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weatherTextView = findViewById(R.id.weatherTextView);
+
+        cityEditText = (EditText) findViewById(R.id.cityEditText);
+        stateEditText = (EditText) findViewById(R.id.stateEditText);
+
     }
 }
